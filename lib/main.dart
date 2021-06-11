20607dc211e509536d9549cc5d3ddaa3fbcf5875
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-11 17:19:41 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("hello"),
+    );
+  }
+}
